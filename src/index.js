c412203e732d9dc0828857df7cd0e3585d63ddc0
Michael <michael@mrplunkett.com>
2021-11-01 21:15:02 -0500

    New books & styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import './scss/style.scss';
-import bookList from './books.csv';
+import bookList from './books.json';
 
 ReactDOM.render(
     <App bookList={bookList} />,
